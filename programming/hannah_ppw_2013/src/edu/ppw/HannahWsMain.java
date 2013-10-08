a41587ad75ca001c489c555043e4fd745444f81f
Hannah <Hannah@192.168.2.26>
2013-10-08 14:49:03 -0400

    First Java class for PPW class

---
+++ programming/hannah_ppw_2013/src/edu/ppw/HannahWsMain.java
@@ -0,0 +1,14 @@
+package edu.ppw;
+
+public class HannahWsMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Mila Olaf Olafson");
+
+		for(int i=0;i < 10;i++){
+			System.out.println("current i ="+i);
+		}
+	}
+
+}
