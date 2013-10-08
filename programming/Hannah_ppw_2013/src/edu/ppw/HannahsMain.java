89dfa49b7ed0038f4733cbdee8720daa4e7dacaf
Student <Student@192.168.2.36>
2013-10-08 14:49:00 -0400

    First Java class for PPW class

---
+++ programming/Hannah_ppw_2013/src/edu/ppw/HannahsMain.java
@@ -0,0 +1,16 @@
+package edu.ppw;
+
+public class HannahsMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world,");
+		System.out.println("I like trains!");
+		
+		for(int i=0; i < 10; i++){
+			System.out.println("Current i = "+i);
+			
+		}
+		
+	}
+}
+
