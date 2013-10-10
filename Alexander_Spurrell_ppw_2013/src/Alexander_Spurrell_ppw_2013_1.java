b10886713ea0920d464e83055cf26f62e9aae96e
aspurrell <aspurrell@gmail.com>
2013-10-10 16:26:49 -0400

    commit my commented java project

--- Alexander_Spurrell_ppw_2013/src/Alexander_Spurrell_ppw_2013_1.java
+++ Alexander_Spurrell_ppw_2013/src/Alexander_Spurrell_ppw_2013_1.java
@@ -2,16 +2,16 @@ import com.neuronrobotics.sdk.dyio.DyIO;
 import com.neuronrobotics.sdk.dyio.peripherals.ServoChannel;
 import com.neuronrobotics.sdk.ui.ConnectionDialog;
 import com.neuronrobotics.sdk.util.ThreadUtil;
-
+// this is the webpage's that have the information to allow the variables to work.
 
 public class Alexander_Spurrell_ppw_2013_1 {
 
 	public static void main(String[] args) {
-		System.out.println("helloWorld");
-		System.out.println("chaChaCha");
+		System.out.println("helloWorld");  //prints helloWorld onto the web. can be made by typing syso and then ctrl space.
+		System.out.println("chaChaCha");   // ditto
 
-		for(int  i=0	;i<10	;i++	){
-			System.out.println("current value of i="+i);
+		for(int  i=0	;i<10	;i++	){  // i++ means i + 1
+			System.out.println("current value of i="+i);  // this change's i
 			
 		}
 
@@ -25,16 +25,16 @@ public class Alexander_Spurrell_ppw_2013_1 {
 		//If your DyIO is using a lower voltage power source, you need to disable the brownout detect
 		dyio.setServoPowerSafeMode(false);
 		
-		ServoChannel srv = new ServoChannel (	dyio.getChannel(11)	);
+		ServoChannel srv = new ServoChannel (	dyio.getChannel(11)	); // this tells the srv what channel to use
 		
-		for(int  i=3	;i<10	;i++	) {
-			srv.SetPosition(i*20, 1);
+		for(int  i=3	;i<10	;i++	) {  //this tells i what to start at, what to end at, and how many to change by
+			srv.SetPosition(i*20, 1); //this tells it how many time to do this per second(s). 
 			
 			ThreadUtil.wait(6000);
 		
 		}
 		
-		dyio.disconnect();
+		dyio.disconnect();   //this ends connection with the dyio
 		
 	}
 	
