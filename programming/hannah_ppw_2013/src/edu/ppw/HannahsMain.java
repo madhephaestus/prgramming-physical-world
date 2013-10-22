c2b7f426dcbd82513d8c4e313e73662df4869320
Hannah <Hannah@192.168.2.6>
2013-10-22 14:54:14 -0400

    Blinking LED and moving the motor.

--- programming/hannah_ppw_2013/src/edu/ppw/HannahsMain.java
+++ programming/hannah_ppw_2013/src/edu/ppw/HannahsMain.java
@@ -15,18 +15,19 @@ public class HannahsMain {
 		}
 		DigitalOutputChannel doc = new DigitalOutputChannel(dyio.getChannel(0));
 		ServoChannel srv = new ServoChannel (dyio.getChannel(11));
+		long longest = 0;
+		long shortest = 1000;
 		// Blink the LED 5 times
-		for(int i = 0; i < 10; i++) {
-			System.out.println("Blinking.");
+		for(int i = 0; i < 20; i++) {
+			long timestamp = System.currentTimeMillis();
 			
 			boolean thisLoopIsOdd = (i % 2) == 1;
 			
 			if(thisLoopIsOdd){
-				System.out.println("This loop is odd "+i);
-				srv.SetPosition(200, 1);
+				
 			}else{
-				System.out.println("This loop is even "+i);
-				srv.SetPosition(50, 1);
+				//System.out.println("This loop is even "+i);
+				srv.SetPosition(50, 0);
 			}
 			
 			// Set the value high every other time, exit if unsuccessful
@@ -36,11 +37,18 @@ public class HannahsMain {
 			}
 			// pause between cycles so that the changes are visible
 			try {
-				Thread.sleep(1000);
+				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			long timePassed= System.currentTimeMillis()-timestamp;
+			if(timePassed>longest)
+					longest=timePassed;
+				if(timePassed<shortest)
+					shortest=timePassed;
+				
+				System.out.println("Time for command to run: "+timePassed/150+" longest: "+longest/150+" shortest: "+shortest/150);
 		}
 		System.exit(0);
 	}
