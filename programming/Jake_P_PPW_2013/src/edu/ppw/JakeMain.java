947d85267bba3848acccc8fcf7a912496360838c
jakepolatty <jakepolatty@gmail.com>
2013-10-29 15:33:29 -0400

    My fixed SevoWrapper.

--- programming/Jake_P_PPW_2013/src/edu/ppw/JakeMain.java
+++ programming/Jake_P_PPW_2013/src/edu/ppw/JakeMain.java
@@ -24,7 +24,9 @@ public class JakeMain {
 		
 
 		
-		ServoChannel srv = new ServoChannel (dyio.getChannel(11));
+		//ServoChannel srv = new ServoChannel (dyio.getChannel(11));
+		 ServoWrapper wrapper = new ServoWrapper(new ServoChannel (dyio.getChannel(11)));
+		 
 		
 		DigitalOutputChannel doc = new DigitalOutputChannel(dyio.getChannel(0));
 		
@@ -38,15 +40,17 @@ public class JakeMain {
 			
 			if(isThisLoopEven){
 				//System.out.println("This loop is even " +i);
-				srv.SetPosition(200, 0);
+				//srv.SetPosition(200, 0);
+				wrapper.setPosition(200,0);
 				doc.setHigh(isThisLoopEven);
 			}else{
 				//System.out.println("This loop is odd " +i);
-				srv.SetPosition(50, 0);
+				//srv.SetPosition(50, 0);
+				wrapper.setPosition(200,0);
 				doc.setHigh(isThisLoopEven);
 			}
 			
-			System.out.println("This loop took " + (System.currentTimeMillis()-startTime)+" ms");
+			System.out.println("This loop took " + (System.currentTimeMillis()-startTime)/150+" ms");
 			//ThreadUtil.wait(6000);
 		}
 		
