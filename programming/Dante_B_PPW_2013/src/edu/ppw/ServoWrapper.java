e79985f27867382844084507a057b66659d56fcf
dantebois19 <dantebois19@gmail.com>
2013-10-29 17:14:34 -0400

    We added a manager for the new servo. We worked with event listeners for the servo wrapper class.

--- programming/Dante_B_PPW_2013/src/edu/ppw/ServoWrapper.java
+++ programming/Dante_B_PPW_2013/src/edu/ppw/ServoWrapper.java
@@ -6,28 +6,40 @@ import com.neuronrobotics.sdk.util.ThreadUtil;
 public class ServoWrapper {
 	
 	private ServoChannel myChannel;
-
+	 
+	private int positionDifferece;
+	
 	public ServoWrapper(ServoChannel myChannel){
    		
 		this.myChannel = myChannel;
 		
 	}
 	
-	public void setPosition(int position,int time){
+	public void setPosition(int position,int time,final EventManager event){
 		
 		int start = myChannel.getValue();
 		
-		int positionDifferece = Math.abs(start-position) * 2; 
+		positionDifferece = Math.abs(start-position) * 2; 
 	
 		myChannel.SetPosition(position, ((double)time)/1000.0 );
 		
+
 		if(time>positionDifferece){
 			positionDifferece = time;
 		}else{
 			System.err.println("I can't move that fast! YOU SCREWED UP! I Need at least "+positionDifferece+"ms");
 		}
-		
-		ThreadUtil.wait(positionDifferece);
+	
+		if(event == null){
+			ThreadUtil.wait(positionDifferece);
+		}else{
+			new Thread(){
+				public void run(){
+					ThreadUtil.wait(positionDifferece);
+					event.onCompletion(myChannel);
+				}
+			}.start();
+		}
 	}
 	
 }
