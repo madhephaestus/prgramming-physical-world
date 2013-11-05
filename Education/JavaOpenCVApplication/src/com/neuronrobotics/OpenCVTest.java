aa07bb8d9cc29a0f37029380e3ea00db2132c8c5
Kevin Harrington <kharrington@neuronrobotics.com>
2013-11-05 15:53:47 -0500

    Resetting repo

--- Education/JavaOpenCVApplication/src/com/neuronrobotics/OpenCVTest.java
+++ Education/JavaOpenCVApplication/src/com/neuronrobotics/OpenCVTest.java
@@ -20,7 +20,6 @@ import com.neuronrobotics.sdk.dyio.peripherals.DigitalOutputChannel;
 import com.neuronrobotics.sdk.dyio.peripherals.ServoChannel;
 import com.neuronrobotics.sdk.ui.ConnectionDialog;
 
-import edu.ppw.ServoEncapsulation;
 
 public class OpenCVTest {
 	int frameX = 640;
@@ -34,58 +33,15 @@ public class OpenCVTest {
 		frame.setSize(frameX, frameY);
 		frame.setVisible(true);
 		frame .setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		DyIO.disableFWCheck();
 		
-		DyIO dyio=new DyIO();
-		if (!ConnectionDialog.getBowlerDevice(dyio)){
-			System.exit(1);
-		}
-		DigitalOutputChannel doc = new DigitalOutputChannel(dyio.getChannel(0));
-		
-		//ServoChannel srv = new ServoChannel (dyio.getChannel(11));
-		
-		
-		ServoEncapsulation panServo = 
-				
-				new ServoEncapsulation(new ServoChannel (dyio.getChannel(11)), 1);
-		
-		ServoEncapsulation tiltServo = 
-				
-				new ServoEncapsulation(new ServoChannel (dyio.getChannel(12)), 1);
-		
-		int panServoPosition =127;
-		int tiltServoPosition =127;
 		while (true){
+			
 			Rect [] faces = faceDetectorObject.getFaces();
 			for(Rect r:faces){
-				//System.out.println("Face at x:"+r.x+" y:"+r.y+" size:"+r.height*r.width);
-				//this is where our servo code goes
-				int centerx = (r.x+r.width/2);
-				int centery = (r.y+r.height/2);
-				System.out.println("The center x = "+centerx+" y = "+centery);
-				
-				int diffx = centerx -(frameX/2);
-				int diffy = centery -(frameY/2);
-				
-				System.out.println("The Difference x = "+diffx+" y = "+diffy);
-				
-				panServoPosition += diffx*-.02;
-				
-				if(panServoPosition>200)
-					panServoPosition=200;
-				if(panServoPosition<50)
-					panServoPosition=50;
-				
-				tiltServoPosition += diffy*.02;
-				
-				if(tiltServoPosition>200)
-					tiltServoPosition=200;
-				if(tiltServoPosition<50)
-					tiltServoPosition=50;
+
+				System.out.println("Face at x:"+r.x+" y:"+r.y+" size:"+r.height*r.width);
+
 			}
-			tiltServo.setPositionTimed(tiltServoPosition, 0);
-			panServo.setPositionTimed(panServoPosition, 0);
-			
 			try {
 				BufferedImage image = faceDetectorObject.getLatestImage();
 				icon.setImage(image);
